9681033575ff1c9190d45aefbbe64bd970d17508
JoelGRod <joelgrod@gmail.com>
2022-03-09 10:38:39 +0000

    custom app bar

--- lib/movies/screens/details_screen.dart
+++ lib/movies/screens/details_screen.dart
@@ -1,15 +1,58 @@
 import 'package:flutter/material.dart';
 
+import 'package:movies_app/shared/themes/themes.dart';
+
 class DetailsScreen extends StatelessWidget {
    
   const DetailsScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
+
+  final String movie = 
+    ModalRoute.of(context)?.settings.arguments.toString() 
+    ?? 'No Movie';
+
     return const Scaffold(
-      body: Center(
-         child: Text('DetailsScreen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
+      )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      // backgroundColor: AppTheme.primaryColor,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          )
+        ),
+        centerTitle: true,
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/imgs/loading.gif'), 
+          image: AssetImage('assets/imgs/loading.gif'),
+          // image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
       ),
+
     );
   }
 }
