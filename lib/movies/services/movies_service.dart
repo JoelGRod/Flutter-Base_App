cba2a2dfabb3d209cac00859b07def51e3369d6e
joelgrod <joelgrod@gmail.com>
2022-03-16 17:01:29 +0000

    secrets are hidden

--- lib/movies/services/movies_service.dart
+++ lib/movies/services/movies_service.dart
@@ -3,13 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:movies_app/movies/models/models.dart';
-
+import 'package:movies_app/shared/secrets/secrets.dart' show MoviesSecrets;
 
 class MoviesService extends ChangeNotifier {
-
-  final String _baseUrl = 'api.themoviedb.org';
-  final String _apiKey = '5c049880e11525b023c95249d3047b17';
-  final String _lang = 'es-ES';
 
   List<Movie> nowPlayingMovies = [];
   List<Movie> popularMovies = [];
@@ -20,38 +16,30 @@ class MoviesService extends ChangeNotifier {
   }
 
   getNowPlayingMovies() async {
-    Uri url = Uri.https(                                                                                                                                       
-      _baseUrl, 
-      '/3/movie/now_playing', 
-      {
-        'api_key': _apiKey,
-        'language': _lang,
-        'page': '1',
-      }
-    );
+    Uri url = Uri.https(MoviesSecrets.baseUrl, '/3/movie/now_playing', {
+      'api_key': MoviesSecrets.apiKey,
+      'language': MoviesSecrets.lang,
+      'page': '1',
+    });
 
     final response = await http.get(url);
-    final NowPlayingRespModel moviesResponse = NowPlayingRespModel.fromJson(response.body);
+    final NowPlayingRespModel moviesResponse =
+        NowPlayingRespModel.fromJson(response.body);
     nowPlayingMovies = moviesResponse.results;
     notifyListeners();
   }
 
   getPopularMovies() async {
-    Uri url = Uri.https(                                                                                                                                       
-      _baseUrl, 
-      '/3/movie/popular', 
-      {
-        'api_key': _apiKey,
-        'language': _lang,
-        'page': '1',
-      }
-    );
+    Uri url = Uri.https(MoviesSecrets.baseUrl, '/3/movie/popular', {
+      'api_key': MoviesSecrets.apiKey,
+      'language': MoviesSecrets.lang,
+      'page': '1',
+    });
 
     final response = await http.get(url);
-    final PopularRespModel moviesResponse = PopularRespModel.fromJson(response.body);
-    popularMovies = [ ...popularMovies, ...moviesResponse.results ];
+    final PopularRespModel moviesResponse =
+        PopularRespModel.fromJson(response.body);
+    popularMovies = [...popularMovies, ...moviesResponse.results];
     notifyListeners();
   }
-
-
 }
