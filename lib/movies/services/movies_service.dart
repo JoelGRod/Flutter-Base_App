9b6202d9d1dee8b9fb3795abf1d4d58f658eb88f
joelgrod <joelgrod@gmail.com>
2022-03-16 19:32:48 +0000

    decoupling http library

--- lib/movies/services/movies_service.dart
+++ lib/movies/services/movies_service.dart
@@ -6,39 +6,39 @@ import 'package:movies_app/movies/models/models.dart';
 import 'package:movies_app/shared/secrets/secrets.dart';
 
 class MoviesService extends ChangeNotifier {
-
   List<Movie> nowPlayingMovies = [];
   List<Movie> popularMovies = [];
+  int _popularPage = 0;
 
   MoviesService() {
     getNowPlayingMovies();
     getPopularMovies();
   }
 
-  getNowPlayingMovies() async {
-    Uri url = Uri.https(MoviesSecrets.baseUrl, '/3/movie/now_playing', {
+  Future<String> _getJsonData(String endpoint, [int page = 1]) async {
+    Uri url = Uri.https(MoviesSecrets.baseUrl, endpoint, {
       'api_key': MoviesSecrets.apiKey,
       'language': MoviesSecrets.lang,
-      'page': '1',
+      'page': '$page',
     });
 
     final response = await http.get(url);
+    return response.body;
+  }
+
+  getNowPlayingMovies() async {
+    final String jsonData = await _getJsonData('/3/movie/now_playing');
     final NowPlayingRespModel moviesResponse =
-        NowPlayingRespModel.fromJson(response.body);
+        NowPlayingRespModel.fromJson(jsonData);
     nowPlayingMovies = moviesResponse.results;
     notifyListeners();
   }
 
   getPopularMovies() async {
-    Uri url = Uri.https(MoviesSecrets.baseUrl, '/3/movie/popular', {
-      'api_key': MoviesSecrets.apiKey,
-      'language': MoviesSecrets.lang,
-      'page': '1',
-    });
+    _popularPage++;
 
-    final response = await http.get(url);
-    final PopularRespModel moviesResponse =
-        PopularRespModel.fromJson(response.body);
+    final String jsonData = await _getJsonData('/3/movie/popular', _popularPage);
+    final PopularRespModel moviesResponse = PopularRespModel.fromJson(jsonData);
     popularMovies = [...popularMovies, ...moviesResponse.results];
     notifyListeners();
   }
